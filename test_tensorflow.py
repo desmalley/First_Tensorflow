b9609c6476d230f85ec6a868a86a46f22e56eab4
Rental <rental@Rental-C1MPW984G944.local>
2023-06-20 10:59:15 -0600

    first commit

---
+++ test_tensorflow.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+from PIL import Image
+import time
+import functools
+
+# Function to load an image and limit its maximum dimension to 512 pixels.
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+# Function to display an image
+def imshow(image, title=None):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
+
+  plt.imshow(image)
+  if title:
+    plt.title(title)
+
+content_path = 'FSY1.PNG'
+style_path = 'Pokemon1.png'
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+plt.subplot(1, 2, 1)
+imshow(content_image, 'Content Image')
+
+plt.subplot(1, 2, 2)
+imshow(style_image, 'Style Image')
+
+# Here comes the style transfer magic
+import tensorflow_hub as tf_hub
+
+# Load the model from TensorFlow Hub
+hub_model = tf_hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
+# Stylize the content image with the style image
+stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+
+# Show the stylized image
+imshow(stylized_image, 'Stylized Image')
+
+plt.show()
